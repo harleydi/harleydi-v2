7ecb3e0219ee9e3a8a71674de94bd796becda254
Yommuu Kumar <yommuukumar@Yommuus-MacBook-Pro.local>
2024-11-27 00:44:07 -0500

    fixed sidenav and other small changes

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -12,14 +12,17 @@ export default function Sidenav() {
       <div className="sidenav">
         <a href="https://twitter.com/bx_giannis">
           <div className='side-hover'>
-            <img src={Twitter} alt='Twitter icon'/>
+            {/* <img src={Twitter} alt='Twitter icon'/> */}
+            <i class="fa-brands fa-twitter"></i>
           </div>
         </a>
         <a href="https://www.linkedin.com/in/disney-harley-a430a4137/">
-          <img src={LinkedIn} alt='LinkedIn icon' />
+          {/* <img src={LinkedIn} alt='LinkedIn icon' /> */}
+          <i class="fa-brands fa-linkedin"></i>
         </a>
         <a href="https://github.com/harleydi">
-          <img src={Github} alt='Github Icon' />
+          {/* <img src={Github} alt='Github Icon' /> */}
+          <i class="fa-brands fa-github"></i>
         </a>
       </div>
     </div>
